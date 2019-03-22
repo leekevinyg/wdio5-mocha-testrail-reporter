1bbc485bd6d050c7d4e845d259ddbcba1620af27
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-03-22 12:36:01 +0900

    Import reporter

--- src/lib/mocha-testrail-reporter.ts
+++ src/lib/mocha-testrail-reporter.ts
@@ -1,112 +1,93 @@
-import {reporters} from 'mocha';
-import {TestRail} from "./testrail";
-import {titleToCaseIds} from "./shared";
-import {Status, TestRailOptions, TestRailResult} from "./testrail.interface";
+let events = require('events');
+import TestRail from "./testrail";
+import {titleToCaseIds} from "./shared"
+import {Status} from "./testrail.interface";
 
-class MochaTestRailReporter extends reporters.Spec {
-    private results: TestRailResult[] = [];
-    private passes: number = 0;
-    private fails: number = 0;
-    private pending: number = 0;
-    private out: string[] = [];
+class WdioTestRailReporter extends events.EventEmitter {
 
-    constructor(runner: any, options: any) {
-        super(runner);
+    /**
+     * @param {{}} baseReporter
+     * @param {{testRailsOptions}} config wdio config
+     */
+    constructor(baseReporter, config) {
+        super();
+        const options = config.testRailsOptions;
 
-        const reporterOptions: TestRailOptions = <TestRailOptions>options.reporterOptions;
-        this.validate(reporterOptions, 'domain');
-        this.validate(reporterOptions, 'username');
-        this.validate(reporterOptions, 'password');
-        this.validate(reporterOptions, 'projectId');
-        this.validate(reporterOptions, 'suiteId');
+        this._results = [];
+        this._passes = 0;
+        this._fails = 0;
+        this._pending = 0;
+        this._out = [];
+        this.testRail = new TestRail(options);
 
-        runner.on('start', () => {
+        this.on('test:pending', (test) => {
+            this._pending++;
+            this._out.push(test.title + ': pending');
         });
 
-        runner.on('suite', (suite) => {
-        });
-
-        runner.on('suite end', () => {
-        });
-
-        runner.on('pending', (test) => {
-            this.pending++;
-            this.out.push(test.fullTitle() + ': pending');
-        });
-
-        runner.on('pass', (test) => {
-            this.passes++;
-            this.out.push(test.fullTitle() + ': pass');
-            const caseIds = titleToCaseIds(test.title);
+        this.on('test:pass', (test) => {
+            this._passes++;
+            this._out.push(test.title + ': pass');
+            let caseIds = titleToCaseIds(test.title);
             if (caseIds.length > 0) {
-                if (test.speed === 'fast') {
-                    let results = caseIds.map(caseId => {
-                        return {
-                            case_id: caseId,
-                            status_id: Status.Passed,
-                            comment: test.title
-                        };
-                    });
-                    this.results.push(...results);
-                } else {
-                    let results = caseIds.map(caseId => {
-                        return {
-                            case_id: caseId,
-                            status_id: Status.Passed,
-                            comment: `${test.title} (${test.duration}ms)`
-                        };
-                    });
-                    this.results.push(...results);
-                }
+                let results = caseIds.map(caseId => {
+                    return {
+                        case_id: caseId,
+                        status_id: Status.Passed,
+                        comment: `${this.getRunComment(test)}`
+                    };
+                });
+                this._results.push(...results);
             }
         });
 
-        runner.on('fail', (test) => {
-            this.fails++;
-            this.out.push(test.fullTitle() + ': fail');
-            const caseIds = titleToCaseIds(test.title);
+        this.on('test:fail', (test) => {
+            this._fails++;
+            this._out.push(test.title + ': fail');
+            let caseIds = titleToCaseIds(test.title);
             if (caseIds.length > 0) {
                 let results = caseIds.map(caseId => {
                     return {
                         case_id: caseId,
                         status_id: Status.Failed,
-                        comment: `${test.title}
-${test.err}`
+                        comment: `${this.getRunComment(test)}
+${test.err.message}
+${test.err.stack}
+`
                     };
                 });
-                this.results.push(...results);
+                this._results.push(...results);
             }
         });
 
-        runner.on('end', () => {
-            if (this.results.length == 0) {
-                console.warn("No testcases were matched. Ensure that your tests are declared correctly and matches TCxxx");
+        this.on('end', () => {
+            if (this._results.length == 0) {
+                console.warn("No testcases were matched. Ensure that your tests are declared correctly and matches TCxxx\n" +
+                  "You may use script generate-cases to do it automatically.");
+                return;
             }
-            const executionDateTime = new Date().toISOString();
-            const total = this.passes + this.fails + this.pending;
-            const name = `Automated test run ${executionDateTime}`;
-            const description = `Automated test run executed on ${executionDateTime}
-Execution summary:
-Passes: ${this.passes}
-Fails: ${this.fails}
-Pending: ${this.pending}
-Total: ${total}
 
-Execution details:
-${this.out.join('\n')}                     
+            let executionDateTime = new Date();
+            let total = this._passes + this._fails + this._pending;
+            let runName = options.runName || WdioTestRailReporter.reporterName;
+            let name = `${runName}: automated test run ${executionDateTime}`;
+            let description = `${name}
+Execution summary:
+Passes: ${this._passes}
+Fails: ${this._fails}
+Pending: ${this._pending}
+Total: ${total}
 `;
-            new TestRail(reporterOptions).publish(name, description, this.results);
+            this.testRail.publish(name, description, this._results);
         });
     }
 
-    private validate(options: TestRailOptions, name: string) {
-        if (options == null) {
-            throw new Error("Missing --reporter-options in mocha.opts");
-        }
-        if (options[name] == null) {
-            throw new Error(`Missing ${name} value. Please update --reporter-options in mocha.opts`);
-        }
+    /**
+     * @param {{title}} test
+     * @return {string}
+     */
+    getRunComment(test) {
+        let comment = test.title;
+        return comment;
     }
 }
-
-export default MochaTestRailReporter;
